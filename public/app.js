116d2ff6f446df63035061cfc17575cf20095729
Bradley Caliva <bcaliva21@gmail.com>
2021-08-30 12:01:59 -0700

    IMplemented functionality to add clicks to the board squares. Removed the placeholder values in the square divs.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+let squares = document.getElementsByClassName('square');
+
+for (const square of squares) {
+  let clickTracker = true;
+  square.addEventListener('click',(e) => {
+    square.innerHTML = (clickTracker) ? 'X' : 'O';
+    clickTracker = !clickTracker;
+  })
+}
+
+console.log(squares);
